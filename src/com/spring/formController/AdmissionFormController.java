24aff1680a0ccc70164045b9d586aaffd0a47c0f
abhinavchmod777 <abhinav.26aug@gmail.com>
2018-07-05 17:45:55 +0530

    using map for handling form

--- src/com/spring/formController/AdmissionFormController.java
+++ src/com/spring/formController/AdmissionFormController.java
@@ -1,5 +1,7 @@
 package com.spring.formController;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,7 +11,7 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class AdmissionFormController 
 {
-	@RequestMapping(value="/AdmissionForm.html"  )
+	@RequestMapping(value="/AdmissionForm.html" , method=RequestMethod.GET )
 	public ModelAndView admission()
 	{
 		ModelAndView obj = new ModelAndView("AdmissionForm");
@@ -17,8 +19,10 @@ public class AdmissionFormController
 	}
 	
 	@RequestMapping(value="/submitForm.html" )
-	public ModelAndView submit(@RequestParam("studentName") String name , @RequestParam("studentHobby") String hobby)
+	public ModelAndView submit(@RequestParam Map<String,String> reqparam)
 	{
+		String name = reqparam.get("studentName");
+		String hobby = reqparam.get("studentHobby");
 		ModelAndView obj = new ModelAndView("Success");
 		obj.addObject("msg", "Your name = "+ name + " hobby = " + hobby );
 		return obj;
